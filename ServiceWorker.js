48d03c3470492eb7367f65a6a5657f3badc5b972
sdnavin <sdr.navin@gmail.com>
2025-03-20 20:29:01 +0300

    update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-FloorPlanner-0.1";
 const contentToCache = [
     "Build/Build.loader.js",
-    "Build/Build.framework.js.gz",
-    "Build/Build.data.gz",
-    "Build/Build.wasm.gz",
+    "Build/Build.framework.js",
+    "Build/Build.data",
+    "Build/Build.wasm",
     "TemplateData/style.css"
 
 ];
